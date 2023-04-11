fbf0a10d74105f625211d37574bbf97ab57f9495
AdanYanes <126865927+AdanYanes@users.noreply.github.com>
2023-04-11 11:40:47 +0100

    feed(MapGenerator): added some static methods

---
+++ movementSimulator/net/Main.java
@@ -0,0 +1,13 @@
+package movementSimulator.net;
+
+import java.util.Arrays;
+
+import movementSimulator.net.salesianos.utils.MapGenerator;
+
+public class Main{
+    public static void main(String[] args) {
+    
+        System.out.println(Arrays.deepToString(MapGenerator.mapGenerator(10)));
+
+    }
+}
---
+++ movementSimulator/net/salesianos/utils/StaticScanner.java
@@ -0,0 +1,6 @@
+package movementSimulator.net.salesianos.utils;
+import java.util.Scanner;
+
+public class StaticScanner {
+    public static Scanner scanner = new Scanner(System.in);
+}
